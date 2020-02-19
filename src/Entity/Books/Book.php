665151a020629cddf2356efd02b00b47572d2a71
Gino Luiggi Chirinos Plasencia <luiggiplasencia0823@gmail.com>
2020-02-19 07:15:27 +0100

    Generar entidades con annotations, yaml y xml

--- src/Entity/Books/Book.php
+++ src/Entity/Books/Book.php
@@ -2,21 +2,66 @@
 
 namespace App\Entity\Books;
 
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Books
+ *
+ * @ORM\Table(name="books")
+ * @ORM\Entity
+ */
 class Book
 {
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer", nullable=false, options={"unsigned"=true})
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="IDENTITY")
+     */
     private $id;
 
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="title", type="string", length=255, nullable=false)
+     */
     private $title;
 
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="description", type="string", length=255, nullable=false)
+     */
     private $description;
 
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="price", type="integer", nullable=false, options={"unsigned"=true})
+     */
     private $price;
 
+    /**
+     * @var int|null
+     *
+     * @ORM\Column(name="author_id", type="integer", nullable=true, options={"unsigned"=true})
+     */
     private $authorId;
 
-    private $createdAt;
+    /**
+     * @var \DateTime
+     *
+     * @ORM\Column(name="created_at", type="datetime", nullable=false, options={"default"="CURRENT_TIMESTAMP"})
+     */
+    private $createdAt = 'CURRENT_TIMESTAMP';
 
-    private $updatedAt;
+    /**
+     * @var \DateTime|null
+     *
+     * @ORM\Column(name="updated_at", type="datetime", nullable=true, options={"default"="CURRENT_TIMESTAMP"})
+     */
+    private $updatedAt = 'CURRENT_TIMESTAMP';
 
     public function getId(): ?int
     {
@@ -94,4 +139,6 @@ class Book
 
         return $this;
     }
+
+
 }
---
+++ src/Entity/Editorials/Editorial.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace App\Entity\Editorials;
+
+class Editorial
+{
+    private $name;
+
+    private $bookId;
+
+    private $createdAt;
+
+    private $updatedAt;
+
+    private $id;
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getBookId(): ?int
+    {
+        return $this->bookId;
+    }
+
+    public function setBookId(int $bookId): self
+    {
+        $this->bookId = $bookId;
+
+        return $this;
+    }
+
+    public function getCreatedAt(): ?\DateTimeInterface
+    {
+        return $this->createdAt;
+    }
+
+    public function setCreatedAt(?\DateTimeInterface $createdAt): self
+    {
+        $this->createdAt = $createdAt;
+
+        return $this;
+    }
+
+    public function getUpdatedAt(): ?\DateTimeInterface
+    {
+        return $this->updatedAt;
+    }
+
+    public function setUpdatedAt(?\DateTimeInterface $updatedAt): self
+    {
+        $this->updatedAt = $updatedAt;
+
+        return $this;
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+}
